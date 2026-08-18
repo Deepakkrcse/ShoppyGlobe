fd2d81ca1fb96730efa3f5ab4cc8a06d8d108dcf
Deepakkrcse <deepakkrcse@gmail.com>
2026-08-18 23:10:32 +0530

    Create reusable ProductItem component

---
+++ src/components/ProductItem.jsx
@@ -0,0 +1,48 @@
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import PropTypes from "prop-types";
+
+import { addToCart } from "../redux/cartSlice";
+
+const ProductItem = ({ product }) => {
+  const dispatch = useDispatch();
+
+  const handleAddToCart = () => {
+    dispatch(addToCart(product));
+  };
+
+  return (
+    <article className="product-card">
+      <img
+        src={product.thumbnail}
+        alt={product.title}
+        loading="lazy"
+      />
+
+      <h3>{product.title}</h3>
+
+      <p>${product.price}</p>
+
+      <div className="card-buttons">
+        <Link to={`/product/${product.id}`}>
+          View
+        </Link>
+
+        <button onClick={handleAddToCart}>
+          Add to Cart
+        </button>
+      </div>
+    </article>
+  );
+};
+
+ProductItem.propTypes = {
+  product: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    thumbnail: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default ProductItem;
