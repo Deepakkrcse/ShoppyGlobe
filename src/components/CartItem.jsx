27bd616817bb9d3b35db9d5e71a525c5c011d5f8
Deepakkrcse <deepakkrcse@gmail.com>
2026-08-18 23:18:26 +0530

    Create reusable CartItem component

---
+++ src/components/CartItem.jsx
@@ -0,0 +1,77 @@
+import { useDispatch } from "react-redux";
+import PropTypes from "prop-types";
+
+import {
+  decreaseQuantity,
+  increaseQuantity,
+  removeFromCart,
+} from "../redux/cartSlice";
+
+const CartItem = ({ item }) => {
+  const dispatch = useDispatch();
+
+  return (
+    <article className="cart-item">
+      <img
+        src={item.thumbnail}
+        alt={item.title}
+        loading="lazy"
+      />
+
+      <div>
+        <h3>{item.title}</h3>
+
+        <p>
+          Price: ${item.price}
+        </p>
+
+        <div className="quantity">
+          <button
+            onClick={() =>
+              dispatch(
+                decreaseQuantity(item.id)
+              )
+            }
+          >
+            -
+          </button>
+
+          <span>{item.quantity}</span>
+
+          <button
+            onClick={() =>
+              dispatch(
+                increaseQuantity(item.id)
+              )
+            }
+          >
+            +
+          </button>
+        </div>
+
+        <button
+          className="remove"
+          onClick={() =>
+            dispatch(
+              removeFromCart(item.id)
+            )
+          }
+        >
+          Remove
+        </button>
+      </div>
+    </article>
+  );
+};
+
+CartItem.propTypes = {
+  item: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    quantity: PropTypes.number.isRequired,
+    thumbnail: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default CartItem;
